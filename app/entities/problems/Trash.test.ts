f3c857636ed1a3dedee48effe2c5f0b82d0180fc
David Whitney <david@davidwhitney.co,uk>
2022-02-22 23:12:00 +0000

    Fixed TS version of Trash test

--- app/entities/problems/Trash.test.ts
+++ app/entities/problems/Trash.test.ts
@@ -1,38 +1,40 @@
-import { problems, entities } from "../../../Config";
+import { problems, entities } from "../../Config";
 const cfg = problems.trash;
 import Trash from "./Trash";
-import Platform from "../Platform";
+import Game from "../../Game";
 
 describe("Mouse", () => {
-    
+
+  let game: Game;
   let trash, platform;
   beforeEach(() => {
-    platform = new Platform("platformId1");
+    game = new Game();
+    platform = game.platform;
     platform.hygiene = 50; // Or the mice try to leave.
 
     trash = new Trash(100, 100);
     trash.isDisplayed = true;
     platform.contents.push(trash);
   });
-  
+
   it("tick - makes platform less hygienic per tick", () => {
-    platform.hygiene = entities.platform.hygieneCap;    
-    
-    trash.tick(platform);
+    platform.hygiene = entities.platform.hygieneCap;
+
+    trash.tick(game);
 
     expect(platform.hygiene).toBe(entities.platform.hygieneCap + cfg.hygieneChangePerTick);
   });
-    
+
   it("tick - spawns a mouse if platform is not hygienic a percentage of the time", () => {
     platform.hygiene = cfg.chanceOfMouseWhenLessThanHygiene;
     trash.random = () => cfg.chanceOfMousePercent;
-    
-    trash.tick(platform);
+
+    trash.tick(game);
 
     expect(trash.spawnedMouse).toBe(true);
     expect(platform.contents.length).toBe(2);
     expect(platform.contents[1].constructor.name).toBe("Mouse");
   });
-  
+
 
 });
